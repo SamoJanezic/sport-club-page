27d26371950eca57911cb4f7d2ebad7c2285a1e5
SamoJanezic <janezic.orion@gmail.com>
2024-03-31 01:34:47 +0100

    added countdown

---
+++ script.js
@@ -0,0 +1,13 @@
+let countDownToDate = new Date("April 1, 2024 00:00:00").getTime();
+setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownToDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
