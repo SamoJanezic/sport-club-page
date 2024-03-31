cb2825fd344d749e00e80fa8ea5e53ddb6855447
SamoJanezic <janezic.orion@gmail.com>
2024-03-31 22:30:52 +0200

    added array option

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const DaysToCountDown = ["April 1,2024 00:00:00"];
+const DaysToCountDown = ["March 31, 2024 22:30:00", "April 2, 2024 00:00:00"];
 
-const countDownToDate = new Date("April 1, 2024 00:00:00").getTime();
+// const countDownToDate = new Date("April 1, 2024 00:00:00").getTime();
+
 setInterval(function() {
     let now = new Date().getTime();
-    let distance = countDownToDate - now;
+    let distance = new Date(DaysToCountDown[0]) - now;
     let days = addLeadingZero(Math.floor(distance / (1000 * 60 * 60 * 24)))
     let hours = addLeadingZero(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     let minutes = addLeadingZero(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
@@ -12,15 +13,14 @@ setInterval(function() {
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
-    // if (distance < 0) {
-        // document.getElementById("countdown").innerHTML = 'Expired';
-    // }
+    if (distance < 0) {
+        DaysToCountDown.shift();
+    }
 }, 1000);
 
 
 function addLeadingZero(number) {
-    console.log(number);
-    if(number<10)
+    if(number < 10)
         return "0" + number;
     else
         return number;
