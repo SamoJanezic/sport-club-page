18b47ea0c435a269014a6bded2cb13946e922717
SamoJanezic <janezic.orion@gmail.com>
2024-03-31 21:39:54 +0200

    script defer and added leading zeor

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const countDownToDate = new Date("March 1, 2024 00:00:00").getTime();
+const DaysToCountDown = ["April 1,2024 00:00:00"];
+
+const countDownToDate = new Date("April 1, 2024 00:00:00").getTime();
 setInterval(function() {
     let now = new Date().getTime();
     let distance = countDownToDate - now;
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = addLeadingZero(Math.floor(distance / (1000 * 60 * 60 * 24)))
+    let hours = addLeadingZero(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    let minutes = addLeadingZero(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    let seconds = addLeadingZero(Math.floor((distance % (1000 * 60)) / 1000));
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
-    if (distance < 0) {
-        document.getElementById("countdown").innerHTML = 'Expired';
-    }
+    // if (distance < 0) {
+        // document.getElementById("countdown").innerHTML = 'Expired';
+    // }
 }, 1000);
+
+
+function addLeadingZero(number) {
+    console.log(number);
+    if(number<10)
+        return "0" + number;
+    else
+        return number;
+}
